1fcf871b347646788ae9b2c21bc9eeadc46108c2
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-01-24 12:48:35 -0600

    Finish find_common_tracks method

--- parsing_itunes_playlists/playlist.py
+++ parsing_itunes_playlists/playlist.py
@@ -7,6 +7,10 @@ def load_plist(filename):
         plist = plistlib.load(fp)
     return plist
 
+def get_track_description(track):
+    name = track['Name']
+    duration = track['Total Time'] // 1000
+    return (name, duration)
 
 def find_duplicates(filename):
     print('Finding duplicate tracks in {}...'.format(filename))
@@ -18,9 +22,7 @@ def find_duplicates(filename):
     track_names = Counter()
     # iterate through the tracks
     for trackId, track in tracks.items():
-        name = track['Name']
-        duration = track['Total Time'] // 1000
-        description = (name, duration)
+        description = get_track_description(track)
         track_names[description] += 1
     # extract duplicates from track_names
     dups = filter(lambda x: track_names[x] > 1, track_names.keys())
@@ -28,13 +30,20 @@ def find_duplicates(filename):
 
 
 def find_common_tracks(filenames):
+    print('Finding common tracks in {}...'.format(filenames))
     # a list of sets of track names
     track_name_sets = []
     for filename in filenames:
         track_names = set()
         plist = load_plist(filename)
-    # LEFT OFF HERE
+        tracks = plist['Tracks']
+        for trackId, track in tracks.items():
+            description = get_track_description(track)
+            track_names.add(description)
+        track_name_sets.append(track_names)
+    return list(set.intersection(*track_name_sets))
 
 
 if __name__ == '__main__':
     print(find_duplicates('test-data/mymusic.xml'))
+    print(find_common_tracks(['test-data/pl1.xml', 'test-data/pl2.xml']))
