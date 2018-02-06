30811133609a123beaa3960951685d8e09f9abc2
Thomas Mooney <tmooney@genome.wustl.edu>
2018-02-06 12:37:12 -0600

    WIP: adding plot_stats

--- parsing_itunes_playlists/playlist.py
+++ parsing_itunes_playlists/playlist.py
@@ -1,5 +1,7 @@
 import plistlib
 from collections import Counter
+import numpy as np
+from matplotlib import pyplot
 
 
 def load_plist(filename):
@@ -43,7 +45,36 @@ def find_common_tracks(filenames):
         track_name_sets.append(track_names)
     return list(set.intersection(*track_name_sets))
 
+def plot_stats(filename):
+    plist = load_plist(filename)
+    tracks = plist['Tracks']
+
+    ratings = [x['Album Rating'] for x in tracks.values()]
+    durations = [x['Total Time'] for x in tracks.values()]
+
+    if not ratings or not durations:
+        print("Sorry, no data in {}".format(filename))
+        return
+
+    x = np.array(durations, np.int32)
+    x = x/60000
+    y = np.array(ratings, np.int32)
+
+    pyplot.subplot(2, 1, 1)
+    pyplot.plot(x, y, 'o')
+    pyplot.axis([0, 1.05*np.max(x), -1, 110])
+    pyplot.xlabel('Track duration')
+    pyplot.ylabel('Track rating')
+
+    pyplot.subplot(2,1,2)
+    pyplot.hist(x, bins=20)
+    pyplot.xlabel('Track duration')
+    pyplot.ylabel('Count')
+
+    pyplot.show()
+
 
 if __name__ == '__main__':
-    print(find_duplicates('test-data/mymusic.xml'))
-    print(find_common_tracks(['test-data/pl1.xml', 'test-data/pl2.xml']))
+    plot_stats('test-data/mymusic.xml')
+#    print(find_duplicates('test-data/mymusic.xml'))
+#    print(find_common_tracks(['test-data/pl1.xml', 'test-data/pl2.xml']))
