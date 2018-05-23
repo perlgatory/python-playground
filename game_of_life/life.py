b520ff1ea741b4a473e8c91b192a5f47bd8b146e
Lee Trani <LMT337@gmail.com>
2018-05-23 12:59:20 -0500

    continued with tutorial

--- game_of_life/life.py
+++ game_of_life/life.py
@@ -1,27 +1,33 @@
 import numpy as np
+import argparse
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
-x = np.array([[0, 0, 255], [255, 255, 0], [0, 255, 0]])
-plt.imshow(x, interpolation='nearest')
-
-plt.show()
-
-# example
-# np.random.choice([0, 255], 4*4, p=[0.1, 0.9]).reshape(4, 4)
-
-
-def add_glider(i, j, grid):
-    """adds a glider with top left cell at (i, j)"""
-    glider = np.array([[0, 0, 255], [255, 0, 255],
-                       [0, 255, 255]])
-    grid[i:i + 3, j:j + 3] = glider
-    return grid
-
 
 ON = 255
 OFF = 0
 
+
+# example
+def random_grid(N):
+    return np.random.choice([OFF, ON], N*N, p=[0.1, 0.9]).reshape(N, N)
+
+
+def add_glider(i, j, grid):
+    """adds a glider with top left cell at (i, j)"""
+    glider = np.array([[OFF, OFF, ON], [ON, OFF, ON],
+                       [OFF, ON, ON]])
+    grid[i:i + 3, j:j + 3] = glider
+    return grid
+
+
+def update(frame_num, img, grid, N):
+    # copy grid since we require 8 neighbors for calculation
+    # and we go line by line
+    new_grid = grid.copy()
+    for i in range(N):
+        for j in range(N):
+            total = int()
+            # TODO rewrite code here, clean it up. PG49
 
 # main() function
 def main():
@@ -38,18 +44,25 @@ def main():
 
     # set grid size
     N = max(args.N, 9)
-    # TODO Start from here next time, page 48
 
     # set animation update interval
-    if args.interval:
-        update_interval = args.interval
+    update_interval = max(args.interval, 50)
 
     # declare grid
     grid = np.array([])
+
     # check if "glider" demo flag is specified
     if args.glider:
         grid = np.zeros(N * N).reshape(N, N)
-        addGlider(1, 1, grid)
+        add_glider(1, 1, grid)
     else:
         # populate grid with random on/off - more off than on
-        grid = randomGrid(N)
+        grid = random_grid(N)
+
+    # set up the animation
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N),
+                                  frames=10,
+                                  interval=update_interval,
+                                  save_count=50)
