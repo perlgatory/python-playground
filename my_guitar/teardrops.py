81e100247fabb4dc885e88993b950882c9d26115
Thomas Mooney <tmooney@genome.wustl.edu>
2018-06-05 12:57:10 -0500

    Placeholder file for next session.

---
+++ my_guitar/teardrops.py
@@ -0,0 +1,8 @@
+import sys, osimport time, random
+import wave, argparse, pygame
+import numpy as npfrom collections
+import dequefrom matplotlib
+import pyplot as plt
+
+#TODO implement a guitar sound (Chapter 4, beginning page 55)
+#TODO start a band to accompany our script
