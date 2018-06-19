e9e01ddada90cf3291da744900827b72af3758dc
Thomas Mooney <tmooney@genome.wustl.edu>
2018-06-19 12:00:26 -0500

    Fix whitespace in "from" for imports.

--- my_guitar/teardrops.py
+++ my_guitar/teardrops.py
@@ -1,7 +1,7 @@
 import sys, osimport time, random
 import wave, argparse, pygame
-import numpy as npfrom collections
-import dequefrom matplotlib
+import numpy as np from collections
+import deque from matplotlib
 import pyplot as plt
 
 #TODO implement a guitar sound (Chapter 4, beginning page 55)
